dcbb6e511268cf1e9fed7918320ee3ec7e096326
Denis <denlnnk2014@gmail.com>
2025-06-13 16:53:25 +0200

    Added: admin displays for models

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.bot.models import TelegramUser
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'user_contact_details', 'status')
+    search_fields = ('username', 'full_name', 'chat_id')
+    readonly_fields = ('created_at', 'updated_at')
--- apps/planner/admin.py
+++ apps/planner/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.planner.models import TaskDirection, Task, TaskDetail
+
+
+@admin.register(TaskDirection)
+class TaskDirectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tg_user')
+    search_fields = ('title', 'tg_user__username', 'tg_user__full_name', 'tg_user__chat_id')
+    autocomplete_fields = ('tg_user',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'day_of_week', 'task_direction', 'tg_user', 'is_active')
+    search_fields = ('day_of_week', 'tg_user__username', 'tg_user__full_name', 'tg_user__chat_id')
+    autocomplete_fields = ('tg_user', 'task_direction',)
+
+
+@admin.register(TaskDetail)
+class TaskDetailAdmin(admin.ModelAdmin):
+    list_display = ('task', 'date', 'status')
+    search_fields = ('task', 'date', 'status')
+    autocomplete_fields = ('task',)
